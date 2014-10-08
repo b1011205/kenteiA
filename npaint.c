68b6d0e0e45cd21c375b7f0f644c87efdb7e644d
Hirotaka_Hanada <b1011205@gmail.com>
2014-10-08 13:19:27 +0900

    お金を計算するkeisan関数を作成しました

--- npaint.c
+++ npaint.c
@@ -1,7 +1,27 @@
 
 #include <stdio.h>
 
+int keisan(int a){
+
+if(a==0)
+return 0;
+if(a == 1 ||a == 2)
+return 1000;
+if(a == 3 || a == 4)
+return 800;
+if(a>=5 && a<= 9)
+return 600;
+if(a>=10 && a<=29)
+return 400;
+if(a>=30)
+return 200;
+
+return -1;
+}
+
 int main(void){
-printf("動作確認です\n");
+int from,until;
+
+scanf("%d\n%d",&from,&until);
 return 0;
 }
